ec4d2c8797051dcf1b4d790e32c19463f2df0021
ShadowSlayer03 <sslayer441@gmail.com>
2026-04-08 23:55:31 +0530

    Add POST handler for view updates using sendBeacon method

--- src/pages/api/views/[postId].ts
+++ src/pages/api/views/[postId].ts
@@ -4,4 +4,5 @@ import putViews from "../_controllers/views/put";
 
 export const GET: APIRoute = (context: APIContext) => getViews(context);
 
-export const PUT: APIRoute = (context: APIContext) => putViews(context);
+// POST handler since sendBeacon always sends POST
+export const POST: APIRoute = (context: APIContext) => putViews(context);
