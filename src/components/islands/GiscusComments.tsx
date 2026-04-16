4c103c0ca14836718be6a054dde9305c8fcb6fd6
ShadowSlayer03 <sslayer441@gmail.com>
2026-04-16 13:26:10 +0530

    Slight change to show Giscus comments in UI

--- src/components/islands/GiscusComments.tsx
+++ src/components/islands/GiscusComments.tsx
@@ -22,9 +22,6 @@ export default function GiscusComments() {
 
   return (
     <div className="mt-12 border-t border-gray-200 pt-8 dark:border-gray-800">
-      <h2 className="mb-6 text-2xl font-bold text-gray-900 dark:text-white">
-        Comments
-      </h2>
       <Giscus
         repo="ShadowSlayer03/Blog-With-Astro"
         repoId="R_kgDORwsoqw"
