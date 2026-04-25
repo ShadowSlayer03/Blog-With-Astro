714888f45acdd49048a31111f2bc709d8c857ec2
ShadowSlayer03 <sslayer441@gmail.com>
2026-04-25 19:51:20 +0530

    Update the likes POST controller to retrieve the database client via `getDb(locals)` rather than importing a module-level `db` instance.

--- src/pages/api/_controllers/likes/post.ts
+++ src/pages/api/_controllers/likes/post.ts
@@ -1,6 +1,6 @@
 import type { APIContext } from "astro";
 import { likes } from "../../../../db/schema";
-import { db } from "../../../../db/client";
+import { getDb } from "../../../../db/client";
 
 const findValuesToInsert = (existingLikesData: any[], postSlugs: string[]) => {
     const existingSlugs = existingLikesData.map((data) => data.post_slug);
@@ -13,7 +13,7 @@ const findValuesToInsert = (existingLikesData: any[], postSlugs: string[]) => {
     }));
 }
 
-const postLikes = async ({ request }: APIContext) => {
+const postLikes = async ({ request, locals }: APIContext) => {
     try {
         const { postSlugs } = await request.json();
 
@@ -25,6 +25,8 @@ const postLikes = async ({ request }: APIContext) => {
                 },
             });
         }
+
+        const db = getDb(locals);
 
         const likeData = await db.select().from(likes);
 
